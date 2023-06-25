c37c7045feee05c55616647defcfc222ab1a021b
z.tizkar <123273835+zizitizi@users.noreply.github.com>
2023-06-25 07:38:39 +0330

    Update containerbackupcron.sh

--- practice/containerbackupcron.sh
+++ practice/containerbackupcron.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#*********please copy this file in /etc/cron.hourly/ to perform auotomated action backup********************
+
 # get all running docker container names
 
 containers=$(sudo docker ps | awk '{if(NR>1) print $NF}')
@@ -10,25 +12,23 @@ host=$(hostname)
 
 #repousername="zeintiz"
 
-reponame="container-backup"
+#reponame="container-backup"
 
-tagname=`date +%Y/%m/%d-%H:%M:%S`
+tagname=`date +%Y%m%d-%H%M%S`
 
-OUTPUT=/home/zizi/
+#OUTPUT=/home/zizi/
 
 for container in $containers
 do
-        docker commit container backup-${container}:${tagname}
-        docker save backup-${container}:${tagname} > ${OUTPUT}backup-${container}:${tagname}.tar
+        echo $container
+        CONNAME=${container}:${tagname}
+        echo $CONNAME
+        docker commit ${container} backup-${CONNAME}
+        docker save backup-${CONNAME} > ~/backup-${CONNAME}.tar
         if [ $? -eq 0 ]
         then
-                 find /home/zizi/ -name "backup-${container}*.tar" -type f -mmin +60 -exec rm -rf {} \;
+                 find ~ -name "backup-${container}*.tar" -type f -mmin +60 -exec rm -rf {} \;
         fi
 done
-
-
-
-
-
 
 
